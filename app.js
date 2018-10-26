1edd5f8726cf6d3b93b78fc56ac9d79e517c0ab0
Jose Carlos Garza Alvarez <jcgarza@marcatel.net>
2018-10-26 18:54:25 -0500

    version 1

---
+++ app.js
@@ -0,0 +1,120 @@
+const express = require('express')
+const sql = require('mssql')
+const app = express()
+const path = require('path')
+const port = process.env.PORT || 3000
+
+// config server
+const config = {
+    user: 'sysdbo',
+    password: 'QxaAvTeG',
+    server: 'mtelsqlsms2', 
+    database: 'SMSReportes' 
+};
+
+// json data
+let Exitosos = {}
+let NoExitosos = {}
+let Campanas = {}
+let monitorProveedores = {}
+let MonitorNoMigrados = {}
+let GraficaProveedor = []
+let GraficaCarrier = []
+let GraficaCliente = []
+let MensajesPrueba = {}
+
+// settings
+app.use('*/css',express.static('src/css'));
+app.use('*/js',express.static('src/js'));
+app.use('*/images',express.static('src/images'));
+app.set('views', path.join(__dirname, 'src/pages'))
+app.set('view engine','ejs')
+
+
+// update Grids every 10 seconds
+setInterval( () => {
+    (async function updateData() {
+        try {
+            let pool = await sql.connect(config)
+            Exitosos = await pool.request()
+                .query('SELECT count(*) as Exitosos FROM smsenvios.dbo.smsenvio with(nolock) where fechaenvio>convert(varchar(10),getdate(),112) and resultado=1')
+            //console.log(qryExitosos.recordset[0].Exitosos)
+            NoExitosos = await pool.request()
+                .query('SELECT count(*) as NoExitosos FROM smsenvios.dbo.smsenvio with(nolock) where fechaenvio>convert(varchar(10),getdate(),112) and resultado=3')
+            
+            Campanas = await pool.request()
+                .query('SELECT count(*) as Campanas FROM  (select distinct idenviopr from SMSCargas.dbo.SMSHorarios with(nolock) where fechainicio>convert(varchar(10),getdate(),112)) as #Campanas')
+            
+            monitorProveedores = await pool.request()
+                .execute('sp_MonitorProveedores')
+
+            MonitorNoMigrados = await pool.request()
+                .execute('sp_MonitorNoMigrados')
+            
+            GraficaProveedor = await pool.request()
+                .execute('sp_graficaproveedorcarrier')
+
+            GraficaCarrier = await pool.request()
+                .execute('sp_graficacarrier')
+            
+            GraficaCliente = await pool.request()
+                .execute('sp_graficaclienteproveedor')
+            
+            MensajesPrueba = await pool.request()
+                .execute('sp_MonitorMensajesPrueba')
+            
+                
+        } catch (err) {
+            console.log(err)
+        }
+        sql.close()
+    })()
+},10000)
+
+
+// Route /Proveedores
+app.get(['/','/Proveedores'], function (req, res) {
+    
+    res.render('proveedores',{
+        monitorProveedores: monitorProveedores,
+        Exitosos: Exitosos.recordset[0].Exitosos,
+        NoExitosos: NoExitosos.recordset[0].NoExitosos,
+        Campanas: Campanas.recordset[0].Campanas
+    });    
+    
+});
+
+// Route /Envios
+app.get('/Envios', function (req, res) {
+    
+    res.render('envios',{
+        MonitorNoMigrados: MonitorNoMigrados
+    });    
+    
+});
+
+// Route /Envios
+app.get('/Graficas', function (req, res) {
+    
+    res.render('graficas',{
+        graficaProveedor: GraficaProveedor.recordset,
+        graficaCarrier: GraficaCarrier.recordset,
+        graficaCliente: GraficaCliente.recordset
+    });    
+    console.log(GraficaProveedor.recordset)
+});
+
+// Route /MensajesPrueba
+app.get('/MensajesPrueba', function (req, res) {
+    
+    res.render('mensajesprueba',{
+        MensajesPrueba: MensajesPrueba,
+        
+    });    
+    console.log(GraficaProveedor.recordset)
+});
+
+// Server Listen
+var server = app.listen(port, function () {
+    console.log(`Server is running on port ${port}`)
+});
---
+++ src/js/Graficos.js
@@ -0,0 +1,310 @@
+﻿
+var chartColors = {
+    red: 'rgb(255, 99, 132)',
+    orange: 'rgb(255, 159, 64)',
+    yellow: 'rgb(255, 205, 86)',
+    green: 'rgb(75, 192, 192)',
+    blue: 'rgb(54, 162, 235)',
+    purple: 'rgb(153, 102, 255)',
+    grey: 'rgb(201, 203, 207)',
+    paleGreen: 'rgb(152,251,152)',
+    lightBlue: 'rgb(173,216,230)',
+    brown: 'rgb(128,0,0)',
+    aqua: 'rgb(0,255,255)',
+    beige: 'rgb(245,245,220)'
+};
+//var prove2 = myResponse[0].Proveedor;
+
+// INICIA GRAFICA PROVEEDOR - CARRIER
+var horas = [];
+var auronixatt = [];
+var auronixtelcel = [];
+var auronixmovistar = [];
+var auronixotros = [];
+//var c3ntroatt = [];
+//var c3ntrotelcel = [];
+//var c3ntromovistar = [];
+//var c3ntrootros = [];
+var quiubasatt = [];
+var quiubastelcel = [];
+var quiubasmovistar = [];
+var quiubasotros = [];
+var connectatt = [];
+var connecttelcel = [];
+var connectmovistar = [];
+var connectotros = [];
+
+for (i = 0; i < graficaProveedor.length; i++) {
+
+    horas.push(graficaProveedor[i].Hora);
+    auronixatt.push(graficaProveedor[i]['Auronix - AT&T']);
+    auronixtelcel.push(graficaProveedor[i]['Auronix - Telcel']);
+    auronixmovistar.push(graficaProveedor[i]['Auronix - Movistar']);
+    auronixotros.push(graficaProveedor[i]['Auronix - Otros']);
+    //c3ntroatt.push(graficaProveedor[i]['C3ntro - AT&T']);
+    //c3ntrotelcel.push(graficaProveedor[i]['C3ntro - Telcel']);
+    //c3ntromovistar.push(graficaProveedor[i]['C3ntro - Movistar']);
+    //c3ntrootros.push(graficaProveedor[i]['C3ntro - Otros']);
+    quiubasatt.push(graficaProveedor[i]['Quiubas - AT&T']);
+    quiubastelcel.push(graficaProveedor[i]['Quiubas - Telcel']);
+    quiubasmovistar.push(graficaProveedor[i]['Quiubas - Movistar']);
+    quiubasotros.push(graficaProveedor[i]['Quiubas - Otros']);
+    connectatt.push(graficaProveedor[i]['Connect - AT&T']);
+    connecttelcel.push(graficaProveedor[i]['Connect - Telcel']);
+    connectmovistar.push(graficaProveedor[i]['Connect - Movistar']);
+    connectotros.push(graficaProveedor[i]['Connect - Otros']);
+};
+
+var configProveedores = {
+    type: 'line',
+    data: {
+        labels: horas,
+        datasets: [
+            //Auronix
+            {
+                label: 'Auronix - AT&T',
+                fill: false,
+                backgroundColor: chartColors.red,
+                borderColor: chartColors.red,
+                data: auronixatt
+            },
+            {
+                label: 'Auronix - Telcel',
+                fill: false,
+                backgroundColor: chartColors.blue,
+                borderColor: chartColors.blue,
+                data: auronixtelcel
+            },
+            {
+                label: 'Auronix - Movistar',
+                fill: false,
+                backgroundColor: chartColors.orange,
+                borderColor: chartColors.orange,
+                data: auronixmovistar
+            },
+            {
+                label: 'Auronix - Otros',
+                fill: false,
+                backgroundColor: chartColors.paleGreen,
+                borderColor: chartColors.paleGreen,
+                data: auronixotros
+            },
+            //C3ntro
+            /*{
+                label: 'C3ntro - AT&T',
+                fill: false,
+                backgroundColor: chartColors.yellow,
+                borderColor: chartColors.yellow,
+                data: c3ntroatt
+            },
+            {
+                label: 'C3ntro - Telcel',
+                fill: false,
+                backgroundColor: chartColors.green,
+                borderColor: chartColors.green,
+                data: c3ntrotelcel
+            },
+            {
+                label: 'C3ntro - Movistar',
+                fill: false,
+                backgroundColor: chartColors.purple,
+                borderColor: chartColors.purple,
+                data: c3ntromovistar
+            },
+            {
+                label: 'C3ntro - Otros',
+                fill: false,
+                backgroundColor: chartColors.grey,
+                borderColor: chartColors.grey,
+                data: c3ntrootros
+            },*/
+            //Quiubas
+            {
+                label: 'Quiubas - AT&T',
+                fill: false,
+                backgroundColor: chartColors.lightBlue,
+                borderColor: chartColors.lightBlue,
+                data: quiubasatt
+            },
+            {
+                label: 'Quiubas - Telcel',
+                fill: false,
+                backgroundColor: chartColors.brown,
+                borderColor: chartColors.brown,
+                data: quiubastelcel
+            },
+            {
+                label: 'Quiubas - Movistar',
+                fill: false,
+                backgroundColor: chartColors.aqua,
+                borderColor: chartColors.aqua,
+                data: quiubasmovistar
+            },
+            {
+                label: 'Quiubas - Otros',
+                fill: false,
+                backgroundColor: chartColors.beige,
+                borderColor: chartColors.beige,
+                data: quiubasotros
+            },
+            //Connect
+            {
+                label: 'Connect - AT&T',
+                fill: false,
+                backgroundColor: chartColors.lightBlue,
+                borderColor: chartColors.lightBlue,
+                data: connectatt
+            },
+            {
+                label: 'Connect - Telcel',
+                fill: false,
+                backgroundColor: chartColors.beige,
+                borderColor: chartColors.beige,
+                data: connecttelcel
+            },
+            {
+                label: 'Connect - Movistar',
+                fill: false,
+                backgroundColor: chartColors.aqua,
+                borderColor: chartColors.aqua,
+                data: connectmovistar
+            },
+            {
+                label: 'Connect - Otros',
+                fill: false,
+                backgroundColor: chartColors.grey,
+                borderColor: chartColors.grey,
+                data: connectotros
+            }
+        ]
+    },
+    options: {
+        responsive: true,
+        title: {
+            display: true,
+            text: 'Proveedor - Carrier',
+            fontFamily: 'sans-serif',
+            fontSize: 20
+        },
+        legend: {
+            position: 'right'
+        }
+    }
+};
+
+var canvasProveedores = document.getElementById("chartProveedores").getContext('2d');
+
+// TERMINA GRAFICA PROVEEDOR - CARRIER
+
+// INICIA GRAFICA DONA CARRIER
+var labelCarrier = [];
+var dataCarrier = [];
+var totalExitosos = 0;
+for (i = 0; i < graficaCarrier.length; i++) {
+    labelCarrier.push(graficaCarrier[i]['CarrierMovil']);
+    dataCarrier.push(graficaCarrier[i]['Cant']);
+    totalExitosos = totalExitosos + graficaCarrier[i]['Cant'];
+};
+var configCarrier = {
+    type: 'doughnut',
+    data: {
+        labels: labelCarrier,
+        datasets: [{
+            backgroundColor: ["#3e95cd", "#8e5ea2", "#3cba9f", "#e8c3b9", "#c45850"],
+            data: dataCarrier
+        }]
+    },
+    options: {
+        title: {
+            display: true,
+            text: 'Mensajes por Carrier',
+            fontFamily: 'sans-serif',
+            fontSize: 20
+        }
+    }
+}
+
+
+var canvasCarrier = document.getElementById("chartCarrier");
+
+//TERMINA GRAFICA PIE CARRIER
+
+// INICIA GRAFICA CLIENTES
+
+var labelClientes = [];
+var dataAuronix = [];
+var dataQuiubas = [];
+var dataC3ntro = [];
+var dataConnect = [];
+
+for (i = 0; i < graficaCliente.length; i++) {
+    labelClientes.push(graficaCliente[i]['Cliente'].substr(0, 30));
+    dataAuronix.push(graficaCliente[i]['Auronix']);
+    dataQuiubas.push(graficaCliente[i]['Quiubas']);
+    dataC3ntro.push(graficaCliente[i]['C3ntro']);
+    dataConnect.push(graficaCliente[i]['Connect']);
+};
+
+var dataClientes = {
+    labels: labelClientes,
+    datasets: [{
+        label: 'Auronix',
+        backgroundColor: window.chartColors.blue,
+        data: dataAuronix
+    }, {
+        label: 'Quiubas',
+        backgroundColor: window.chartColors.red,
+        data: dataQuiubas
+    }, {
+        label: 'C3ntro',
+        backgroundColor: window.chartColors.green,
+        data: dataC3ntro
+    }, {
+        label: 'Connect',
+        backgroundColor: window.chartColors.purple,
+        data: dataConnect
+    }]
+};
+
+var configClientes = {
+    type: 'bar',
+    data: dataClientes,
+    options: {
+        title: {
+            display: true,
+            text: 'Clientes - Proveedor',
+            fontFamily: 'sans-serif',
+            fontSize: 20
+
+        },
+        tooltips: {
+            mode: 'index',
+            intersect: false
+        },
+        responsive: true,
+        scales: {
+            xAxes: [{
+                stacked: true,
+                ticks: {
+                    autoSkip: false,
+                    maxRotation: 90,
+                    minRotation: 90,
+                    fontSize: 10
+                }
+            }],
+            yAxes: [{
+                stacked: true
+            }]
+        }
+    }
+}
+
+var canvasClientes = document.getElementById('chartClientes').getContext('2d');
+
+
+// TERMINA GRAFICA CLIENTES
+
+// Renderiza Graficas
+var myDoughnutChart = new Chart(canvasCarrier, configCarrier);
+var myLineChart = new Chart(canvasProveedores, configProveedores);
+var myBarChart = new Chart(canvasClientes, configClientes);
