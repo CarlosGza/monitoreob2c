93f01d5211b73209f33183d7ed820060877b741a
Jose Carlos Garza Alvarez <jcgarza@marcatel.net>
2018-10-27 03:48:23 -0500

    new routes

--- app.js
+++ app.js
@@ -27,47 +27,7 @@ app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-/*
-// update Grids every 10 seconds
-setInterval( () => {
-    (async function updateData() {
-        try {
-            let pool = await sql.connect(config)
-            Exitosos = await pool.request()
-                .query('SELECT count(*) as Exitosos FROM smsenvios.dbo.smsenvio with(nolock) where fechaenvio>convert(varchar(10),getdate(),112) and resultado=1')
-            //console.log(qryExitosos.recordset[0].Exitosos)
-            NoExitosos = await pool.request()
-                .query('SELECT count(*) as NoExitosos FROM smsenvios.dbo.smsenvio with(nolock) where fechaenvio>convert(varchar(10),getdate(),112) and resultado=3')
-            
-            Campanas = await pool.request()
-                .query('SELECT count(*) as Campanas FROM  (select distinct idenviopr from SMSCargas.dbo.SMSHorarios with(nolock) where fechainicio>convert(varchar(10),getdate(),112)) as #Campanas')
-            
-            monitorProveedores = await pool.request()
-                .execute('sp_MonitorProveedores')
-
-            MonitorNoMigrados = await pool.request()
-                .execute('sp_MonitorNoMigrados')
-            
-            GraficaProveedor = await pool.request()
-                .execute('sp_graficaproveedorcarrier')
-
-            GraficaCarrier = await pool.request()
-                .execute('sp_graficacarrier')
-            
-            GraficaCliente = await pool.request()
-                .execute('sp_graficaclienteproveedor')
-            
-            MensajesPrueba = await pool.request()
-                .execute('sp_MonitorMensajesPrueba')
-            
-                
-        } catch (err) {
-            console.log(err)
-        }
-        sql.close()
-    })()
-},10000)
-*/
+// Route to post data
 app.post('/',function (req, res){
     Exitosos = req.body.Exitosos
     NoExitosos = req.body.NoExitosos
@@ -78,50 +38,58 @@ app.post('/',function (req, res){
     GraficaCarrier = req.body.GraficaCarrier
     GraficaCliente = req.body.GraficaCliente
     MensajesPrueba = req.body.MensajesPrueba
-    res.end("200")
+    res.send('200')
 });
 
-// Route /Envios
-app.get('/Programados', function (req, res) {
-    
-    res.render('programados',{
-        MonitorNoMigrados: MonitorNoMigrados    
-    });
-});
 
 // Route /Proveedores
 app.get(['/','/Proveedores'], function (req, res) {
-    
-    res.render('proveedores',{
-        monitorProveedores: monitorProveedores,
-        Exitosos: Exitosos.recordset[0].Exitosos,
-        NoExitosos: NoExitosos.recordset[0].NoExitosos,
-        Campanas: Campanas.recordset[0].Campanas
-    });    
-    
+    if (Object.keys(monitorProveedores).length > 0 && Object.keys(Exitosos).length > 0 && Object.keys(NoExitosos).length > 0 && Object.keys(Campanas).length > 0){
+        res.render('proveedores',{
+            monitorProveedores: monitorProveedores,
+            Exitosos: Exitosos.recordset[0].Exitosos,
+            NoExitosos: NoExitosos.recordset[0].NoExitosos,
+            Campanas: Campanas.recordset[0].Campanas
+        });    
+    } else {
+        res.send('Sin datos cargados')
+    }
 });
 
-
+// Route /Programados
+app.get('/Programados', function (req, res) {
+    if (Object.keys(MonitorNoMigrados).length > 0){
+        res.render('programados',{
+            MonitorNoMigrados: MonitorNoMigrados    
+        });
+    } else {
+        res.send('Sin datos cargados')
+    }
+});
 
 // Route /Graficas
 app.get('/Graficas', function (req, res) {
+    if (GraficaProveedor.length > 0 && GraficaCarrier.length > 0 && GraficaCliente.length > 0){
+        res.render('graficas',{
+            graficaProveedor: GraficaProveedor.recordset,
+            graficaCarrier: GraficaCarrier.recordset,
+            graficaCliente: GraficaCliente.recordset
+        });    
+    } else {
+        res.send('Sin datos cargados')
+    }
     
-    res.render('graficas',{
-        graficaProveedor: GraficaProveedor.recordset,
-        graficaCarrier: GraficaCarrier.recordset,
-        graficaCliente: GraficaCliente.recordset
-    });    
-    console.log(GraficaProveedor.recordset)
 });
 
 // Route /MensajesPrueba
 app.get('/MensajesPrueba', function (req, res) {
-    
-    res.render('mensajesprueba',{
-        MensajesPrueba: MensajesPrueba,
-        
-    });    
-    console.log(GraficaProveedor.recordset)
+    if (Object.keys(MensajesPrueba).length > 0){
+        res.render('mensajesprueba',{
+            MensajesPrueba: MensajesPrueba
+        });    
+    } else {
+        res.send('Sin datos cargados')
+    }
 });
 
 // Server Listen
