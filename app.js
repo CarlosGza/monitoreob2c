dab834bcf7096f9db0fce106b805b39846a4984b
Jose Carlos Garza Alvarez <jcgarza@vivaro.com>
2022-07-13 17:55:53 -0500

    se cambia conexion a qa

--- app.js
+++ app.js
@@ -436,7 +436,7 @@ app.get('*', (req, res) => {
 
 // Server Listen
 ;(async () => {
-	pool = new sql.ConnectionPool(db.prod)
+	pool = new sql.ConnectionPool(db.qa)
 	await pool.connect()
 	app.listen(port, function () {
 		console.log(`Server is running on port ${port}`)
